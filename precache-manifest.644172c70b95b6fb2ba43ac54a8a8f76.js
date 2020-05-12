3779aa0b62d7cf91f1006547ef0f7d357149eaa7
ShapovalDenys <denys.igorevich@gmail.com>
2020-05-12 17:10:56 +0300

    Updates

---
+++ precache-manifest.644172c70b95b6fb2ba43ac54a8a8f76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376edb6074aeb1e68b249b1a4c610917",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2229df4b8dee638face8",
+    "url": "./static/css/main.eb42ddda.chunk.css"
+  },
+  {
+    "revision": "cc91cfdee7c267f57e15",
+    "url": "./static/js/2.b70c53f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b70c53f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2229df4b8dee638face8",
+    "url": "./static/js/main.82ea2139.chunk.js"
+  },
+  {
+    "revision": "cc951e334f15251cfce1",
+    "url": "./static/js/runtime-main.88190073.js"
+  }
+]);
